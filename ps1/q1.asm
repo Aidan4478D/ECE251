275554a8c7ccde60db517a7b05b8e2f05bd096f3
Aidan Cusa <aidan.cusa@cooper.edu>
2024-03-06 20:49:22 -0500

    Added hw2 and directory for ps1

---
+++ ps1/q1.asm
@@ -0,0 +1,31 @@
+.data
+### Setup Test Input ###
+    A: .word 9, 8, 7, 6, 5
+    B: .word 0, 1, 2, 3, 4
+    g: .word 5
+
+.text
+.globl main
+main:
+### Load Data Values Into Registers ###
+    lw $s1, g
+    la $s6, A
+    la $s7, B
+    
+### Your code for Q1 goes here ###
+    
+    lw $t0, 16($s7)
+    sll $t0, $t0, 2
+    add $t0, $t0, $s6
+    lw $t0, 0($t0)
+    sub $s0, $s1, $t0
+
+
+### Print S0 Register ###
+    move $a0, $s0
+    li $v0, 1
+    syscall
+
+### Exit Program ###
+    li $v0, 10
+    syscall
